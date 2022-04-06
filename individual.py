0ac1d1a1636278072c69e75dbbe9f49785458278
danilaple <denlex76@gmail.com>
2022-04-06 08:33:38 +0300

    program: individual

---
+++ individual.py
@@ -0,0 +1,6 @@
+import math
+a=int(input("Enter the length of the larger base a: "))
+b=int(input("Enter the length of the smaller base b: "))
+alpha=int(input("Enter the angle at a larger base in degrees: "))
+alphar=alpha*math.pi/180
+print("The area of the trapezoid is equal to ",(a**2-b**2)/4*math.tan(alphar))
